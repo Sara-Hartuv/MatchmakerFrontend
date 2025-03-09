e998704839128cfb708413c8c4eee4d5b02e583b
sara hartuv <sarahartuv@gmail.com>
2025-03-09 21:26:18 +0200

    create the project in fronted

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/router";  // טוענים את ה-router שהגדרת
+import InitializeAuth from "./auth/InitializeAuth";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <InitializeAuth>
+        <RouterProvider router={router} />
+      </InitializeAuth>
+    </Provider>
   );
 }
 
